61f3b084603524083dfe64bc43288c7023bbe8d7
Jethro Carr <jcarr@sailthru.com>
2016-11-29 11:11:31 +1300

    Add validation of AWS instance metadata
    We now validate tags, launch time and instance name against the cert
    name. Have implemented automagic mapping of the puppet extensions to
    tags on the instances.

--- autosign-puppet-aws.rb
+++ autosign-puppet-aws.rb
@@ -16,7 +16,7 @@ require 'aws-sdk'
 # own libraries to crunch the CSR and extract out any values that have been
 # defined.
 
-puts "[autosign] Processing supplied CSR..."
+puts "[autosign] Processing supplied CSR (from STDIN)..."
 
 clientcert = ARGV.pop
 csr = Puppet::SSL::CertificateRequest.from_s(STDIN.read)
@@ -57,14 +57,84 @@ instance_details = client_ec2.describe_instances({
   dry_run: false,
 })
 
-# Validate attributes
+if instance_details.reservations.empty?
+  puts "[autosign] Failing signing as instance does not exist."
+  exit 1
+end
+
+
+# Ensure that we are trying to sign a running instance. Not much use signing
+# anything that has already been terminated...
 unless instance_details.reservations[0].instances[0].state.name == 'running'
   puts "[autosign] Failing signing as instance not running (current state: #{instance_details.reservations[0].instances[0].state.name})"
   exit 1
 end
 
-# TODO: Convert and validate launch time.
-puts instance_details.reservations[0].instances[0].launch_time
 
-# TODO: Validate tags against puppet params
-puts instance_details.reservations[0].instances[0].tags
+# Validate that the instance has a launch time less than 1800 seconds. This
+# additional check is to ensure if an attacker has been able to manipulate EC2
+# tags, they are restricted to a tight time window when new servers are
+# provisioned.
+if (Time.now.utc - instance_details.reservations[0].instances[0].launch_time) > 1800
+  puts "[autosign] Failing signing as instance was launched more than 30mins ago, refusing to sign cert."
+  exit 1
+end
+
+
+# Validate the instance name against the Name tag on the EC2 instance. We strip
+# any FQDN extension and compare the hostname only.
+instance_name_cert = clientcert.split('.')[0]
+
+validated = false
+instance_details.reservations[0].instances[0].tags.each do |tag|
+  if tag["key"].downcase == 'name'
+    instance_name_tag = tag["value"].downcase.split('.')[0]
+
+    if instance_name_cert == instance_name_tag
+      validated = true
+    else
+      puts "[autosign] Failing signing as certname #{instance_name_cert} does not match name tag with value of #{tag["value"]}."
+      exit 1
+    end
+  end
+end
+
+if validated == false
+  puts "[autosign] Failed signing as tag Name does not exist."
+  exit 1
+end
+
+
+# Iterate through the Puppet extended values (other than instance_id and region)
+ csr_extensions.each do |extension, value|
+   unless extension == 'pp_instance_id' or extension == 'pp_region'
+     # Drop the pp_ prefix from the extension value.
+     extension = extension[3, extension.length].downcase
+     value = value.downcase
+
+     # Iterate through the tags. We lowercase the tag and values to avoid case
+     # sensitivity headaches.
+     validated = false
+     instance_details.reservations[0].instances[0].tags.each do |tag|
+       if tag["key"].downcase == extension
+         puts "[autosign] Validating tag #{extension} (expected value: #{value})"
+         if tag["value"].downcase == value
+           validated = true
+         else
+           puts "[autosign] Failing signing as tag #{extension} value is #{tag["value"]} rather than expected #{value}"
+           exit 1
+         end
+       end
+     end
+
+     if validated == false
+       puts "[autosign] Failed signing as tag #{extension} does not exist."
+       exit 1
+     end
+   end
+ end
+
+
+# We passed the gauntlet! Approve certificate for signing.
+puts "[autosign] All validations passed, certificate #{clientcert} approved."
+exit 0
